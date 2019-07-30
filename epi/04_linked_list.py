18c2eba5520421445bef68c01d389995f4b25776
Jason Ding <dingyue0626@gmail.com>
2019-07-29 23:36:03 -0400

    7.4, 7.5

--- epi/04_linked_list.py
+++ epi/04_linked_list.py
@@ -32,13 +32,45 @@ def print_list(L, threadshold=20):
     result.append('#')
     print(''.join(result))
 
-def make_cycle_at(L, n):
+def make_cycle_at(L, k):
     last = p = L
     while last.next:
         last = last.next
-    for _ in range(1, n):
+    for _ in range(1, k):
         p = p.next
     last.next = p
+
+def list_len(L):
+    p, length = L, 0
+    while p:
+        length += 1
+        p = p.next
+    return length
+
+def get_last_node(L):
+    p = L
+    while p.next:
+        p = p.next
+    return p
+
+def get_node_at(L, k):
+    p = L
+    for _ in range(1, k):
+        p = p.next
+    return p
+
+def distance(start, end):
+    steps = 0
+    while start is not end:
+        start = start.next
+        steps += 1
+    return steps
+
+def make_overlap_at(L1, L2, k):
+    last_node = get_last_node(L1)
+    if last_node:
+        last_node.next = get_node_at(L2, k)
+
 
 """ 7.1 MERGE TWO SORTED  LISTS
 
@@ -136,31 +168,18 @@ def has_cycle3(L):
             return True
     return False
 
-def has_cycle_at(L):
-    def cycle_len(end):
-        start, step = end, 0
-        while True:
-            step += 1
-            start = start.next
-            if start is end:
-                return step
-
+def get_cycle_root(L):
     fast = slow = L
     while fast and fast.next and fast.next.next:
         slow = slow.next
         fast = fast.next.next
         if slow is fast: # found cycle
-            # Find the meeting point
-            meet_point = L
-            for _ in range(cycle_len(slow)):
-                meet_point = meet_point.next
             # Move both cycle_head and meet_point in tandem
-            cycle_head = L
-            while cycle_head is not meet_point:
-                cycle_head = cycle_head.next
-                meet_point = meet_point.next
-            return cycle_head
-
+            slow = L
+            while slow is not fast:
+                slow = slow.next
+                fast = fast.next
+            return fast
     return None # No cycle
 
 """ 7.4 TEST FOR OVERLAPPING LISTS - LISTS ARE CYCLE-FREE
@@ -168,3 +187,64 @@ def has_cycle_at(L):
     Write a program that takes two cycle-free singly linked list, and determines
     if there exists a node that is common to both lists.
 """
+def overlap_no_cycle(L1, L2):
+    len1, len2 = list_len(L1), list_len(L2)
+    if len1 > len2:
+        L1, L2 = L2, L1 # L2 is always the longer list
+
+    # advances the longer list to get equal length lists.
+    for _ in range(abs(len1 - len2)):
+        L2 = L2.next
+
+    while L1 and L2 and L1 is not L2:
+        L1, L2 = L1.next, L2.next
+
+    return L1 # None implies there is no overlap between L1 and L2
+
+""" 7.5 TEST FOR OVERLAPPING LISTS - LISTS MAY HAVE CYCLES
+
+    Solve the problem 7.4 for the case where the lists may each or both have a
+    cycle.
+"""
+def overlap_list(L1, L2):
+    # store the start of cycle if any.
+    root1, root2 = get_cycle_root(L1), get_cycle_root(L2)
+
+    if not root1 and not root2:
+        # Both lists don't have cycle
+        return overlap_no_cycle(L1, L2)
+    elif (root1 and not root2) or (root2 and not root1):
+        # One list has cycle, the other one has no cycle
+        return None # they cannot be overlapping
+
+    # Both lists have cycles
+
+    # L1 and L2 do not end in the same cycle.
+    if root1 is not root2:
+        return None # cycles are disjoint
+
+    # L1 and L2 end in the same cycle, locate the overlapping node if they
+    # first overlap before cycle starts.
+    stem1_len, stem2_len = distance(L1, root1), distance(L2, root2)
+    if stem1_len > stem2_len:
+        L1, L2 = L2, L1 # L2 is always the longer list
+        root1, root2 = root2, root1
+
+    for _ in range(abs(stem2_len - stem1_len)):
+        L2 = L2.next
+
+    while L1 is not L2 and L1 is not root1 and L2 is not root2:
+        L1, L2 = L1.next, L2.next
+
+    # If L1 == L2 before reaching root1, it means the overlap first occurs
+    # before the cycle starts; otherwise, the first overlapping node is not
+    # unique, we can return any node on the cycle.
+    return L1 if L1 is L2 else root1
+
+L1 = create_list([1,2])
+L2 = create_list([3,4,5,6])
+make_cycle_at(L2, 2)
+make_overlap_at(L1, L2, 2)
+print_list(L1)
+print_list(L2)
+print_list(overlap_list(L1, L2))
